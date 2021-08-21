0dc343e14c8ea44de754770a971e4bdc993401e5
Malavika K <malu4353@gmail.com>
2021-08-21 09:39:46 +0530

    Initial checkin

---
+++ src/main/java/com/malavika/assignment/malavika_maven_assignment/App.java
@@ -0,0 +1,13 @@
+package com.malavika.assignment.malavika_maven_assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to maven assignment Malavika!" );
+    }
+}
